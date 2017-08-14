7331c54e0d28bac57f8567b0be873f34796435f0
Andrew Karnani <andrew@akarnani.com>
2017-08-13 20:55:06 -0700

    working https redirect

---
+++ redirect.go
@@ -0,0 +1,21 @@
+package main
+import (
+    "net/http"
+    "log"
+)
+
+func redirect(w http.ResponseWriter, req *http.Request) {
+    // remove/add not default ports from req.Host
+    target := "https://" + req.Host + req.URL.Path 
+    if len(req.URL.RawQuery) > 0 {
+        target += "?" + req.URL.RawQuery
+    }
+    http.Redirect(w, req, target,
+            // see @andreiavrammsd comment: often 307 > 301
+            http.StatusTemporaryRedirect)
+}
+
+func main() {
+    // redirect every http request to https
+    go http.ListenAndServe(":80", http.HandlerFunc(redirect))
+}
